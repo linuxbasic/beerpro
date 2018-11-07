963488dbe5b6c08b7284cd3985787fd847f2f393
Linus Basig <linus@basig.me>
2018-11-07 10:49:57 +0100

    Add category filter chips

--- app/src/main/java/ch/beerpro/presentation/explore/search/SearchActivity.java
+++ app/src/main/java/ch/beerpro/presentation/explore/search/SearchActivity.java
@@ -4,23 +4,34 @@ import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
 import ch.beerpro.R;
 import ch.beerpro.domain.models.Beer;
+import ch.beerpro.domain.models.MyBeer;
 import ch.beerpro.presentation.details.DetailsActivity;
 import ch.beerpro.presentation.explore.search.beers.SearchResultFragment;
 import ch.beerpro.presentation.explore.search.suggestions.SearchSuggestionsFragment;
 import ch.beerpro.presentation.profile.mybeers.MyBeersViewModel;
 import ch.beerpro.presentation.profile.mybeers.OnMyBeerItemInteractionListener;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.tabs.TabLayout;
 import com.google.common.base.Strings;
+
+import java.util.HashSet;
+import java.util.List;
+
 
 public class SearchActivity extends AppCompatActivity
         implements SearchResultFragment.OnItemSelectedListener, SearchSuggestionsFragment.OnItemSelectedListener,
@@ -31,6 +42,7 @@ public class SearchActivity extends AppCompatActivity
     private EditText searchEditText;
     private MyBeersViewModel myBeersViewModel;
     private TabLayout tabLayout;
+    private ChipGroup filterChips;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +64,7 @@ public class SearchActivity extends AppCompatActivity
             handleSearch(null);
         });
 
+        filterChips = findViewById(R.id.filterChips);
         ViewPager viewPager = findViewById(R.id.viewpager);
         tabLayout = findViewById(R.id.tablayout);
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -60,6 +73,7 @@ public class SearchActivity extends AppCompatActivity
         viewPager.setSaveFromParentEnabled(false);
         searchViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
         myBeersViewModel = ViewModelProviders.of(this).get(MyBeersViewModel.class);
+        searchViewModel.getFilteredBeers().observe(this, this::updateFilters);
     }
 
     private void handleSearch(String text) {
@@ -67,6 +81,23 @@ public class SearchActivity extends AppCompatActivity
         myBeersViewModel.setSearchTerm(text);
         adapter.setShowSuggestions(Strings.isNullOrEmpty(text));
         adapter.notifyDataSetChanged();
+    }
+
+    private void updateFilters(List<Beer> beers){
+        HashSet<String> categories = new HashSet();
+
+        filterChips.removeAllViews();
+
+        for (Beer beer : beers){
+            categories.add(beer.getCategory());
+        }
+
+        for (String category : categories){
+            Chip categoryChip = new Chip(filterChips.getContext());
+            categoryChip.setText(category);
+            categoryChip.setCheckable(true);
+            filterChips.addView(categoryChip);
+        }
     }
 
     private void addSearchTermToUserHistory(String text) {
